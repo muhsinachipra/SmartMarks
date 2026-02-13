3177b9e4798c141868bf672747f6663fde7cc7ce
muhsin <muhsin@zoptal.in>
2026-02-13 18:26:23 +0530

    feat: create root layout for the application, including metadata, fonts, and basic styling.

--- app/layout.tsx
+++ app/layout.tsx
@@ -15,7 +15,7 @@ export default function RootLayout({
   children: React.ReactNode;
 }>) {
   return (
-    <html lang="en" className="dark">
+    <html lang="en" className="dark" suppressHydrationWarning>
       <head>
         <link href="https://fonts.googleapis.com/icon?family=Material+Icons" rel="stylesheet" />
         <link
